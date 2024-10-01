7d1c640bb36241a850f17ba12e643e5fdc480323
asifalam515 <asibulalam515@gmail.com>
2024-10-01 10:14:42 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+var cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// q1yUOAk9pGvA1VUU
+// tourism_hub
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6tngyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
